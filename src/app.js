82e3853158cdacdf81d316ba934180f6543347e2
Radosław <radoslaw.biesek@gmail.com>
2020-04-04 19:09:50 +0200

    Add middlewares for error handling and setting headers

--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const setHeaders = require('./middleware/setHeaders');
+const errorHandler = require('./middleware/errorHandler');
+
 const app = express();
 
 app.use(bodyParser.json());
+
+app.use(setHeaders);
+app.use(errorHandler);
 
 app.listen(8080, () => console.log('App is listening on port 8080.'));
---
+++ src/middleware/errorHandler.js
@@ -0,0 +1,6 @@
+module.exports = (error, req, res, next) => {
+  const statusCode = error.statusCode || 500;
+  const message = error.message;
+  const data = error.data;
+  res.status(statusCode).json({ message, data });
+};
