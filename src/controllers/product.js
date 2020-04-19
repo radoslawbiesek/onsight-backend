d7b7a47fcb17f6d4377c3a51e3b78e49b193bed1
Radosław <radoslaw.biesek@gmail.com>
2020-04-19 19:08:48 +0200

    Add filtering to GET /products. Refactor GET /filters controller

--- src/controllers/product.js
+++ src/controllers/product.js
@@ -2,6 +2,8 @@ const Product = require('../models/product');
 
 const HTTPError = require('../errors/httpError');
 
+const FILTERS = ['brand', 'category', 'color', 'size'];
+
 exports.createProduct = async (req, res, next) => {
   try {
     const { name, desc, price, img, category, size, brand, color } = req.body;
@@ -43,15 +45,26 @@ exports.getProduct = async (req, res, next) => {
 
 exports.getProducts = async (req, res, next) => {
   try {
-    const offset = +req.query.offset || null;
-    const limit = +req.query.limit || null;
-    const sort = req.query.sort || 'name';
-    const products = await Product.find()
+    const {
+      offset = null,
+      limit = null,
+      sort = 'name',
+      ...queryFilters
+    } = req.query;
+
+    let filters = {};
+    Object.keys(queryFilters).forEach((key) => {
+      if (FILTERS.includes(key) && queryFilters[key] !== '') {
+        filters[key] = { $in: queryFilters[key].split(',') };
+      }
+    });
+
+    const products = await Product.find(filters)
       .sort(sort)
-      .skip(offset)
-      .limit(limit)
+      .skip(+offset)
+      .limit(+limit)
       .select('_id name price img category size brand');
-    const count = await Product.countDocuments();
+    const count = await Product.countDocuments(filters);
     res.status(200).json({
       message: 'Products found',
       data: { products, count },
@@ -66,16 +79,29 @@ exports.getFilters = async (req, res, next) => {
   try {
     const products = await Product.find(
       {},
-      { category: 1, color: 1, size: 1, brand: 1 }
+      { category: 1, color: 1, size: 1, brand: 1, _id: 0 }
     );
-    const brand = Array.from(new Set(products.map(({ brand }) => brand)));
-    const size = Array.from(new Set(products.map(({ size }) => size)));
-    const category = Array.from(new Set(products.map(({ category }) => category)));
-    const color = Array.from(new Set(products.map(({ color }) => color).flat()));
+    const data = products.reduce(
+      (acc, product) => {
+        FILTERS.forEach((filter) => {
+
+          acc[filter] = Array.isArray(product[filter])
+            ? Array.from(new Set([...acc[filter], ...product[filter]]))
+            : Array.from(new Set([...acc[filter], product[filter]]));
+        });
+        // acc.brand = Array.from(new Set([...acc.brand, product.brand]));
+        // acc.category = Array.from(new Set([...acc.category, product.category]));
+        // acc.color = Array.from(new Set([...acc.color, ...product.color]));
+        // acc.size = Array.from(new Set([...acc.size, product.size]));
+        return acc;
+      },
+      { brand: [], category: [], color: [], size: [] }
+    );
+
     res.status(200).json({
       message: 'Categories found',
       error: '',
-      data: { brand, size, category, color },
+      data,
     });
   } catch (error) {
     next(error);
